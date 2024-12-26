6bebfb28fe4ee262045bae94fd1f200cfa1f2881
Klaus <ansichrist@yandex.ru>
2024-12-26 19:17:50 +0300

    add datetime convertion

--- main.c
+++ main.c
@@ -38,8 +38,44 @@ static void test_timer(void){
 
 
 
+
+static void test_localtime(void){
+    struct timespec t;
+    struct tm *d;
+    time_t x;
+    
+    timespec_runtime(&t,
+        unsigned int i=1000000;
+        while(i--){
+            x=time(NULL);
+            d=localtime(&x);
+        }
+    );
+    
+    printf("localtime %f ms: %d.%d.%d / %d:%d:%d / %d / %d\n",timespec_milliseconds(&t),
+    d->tm_mday,d->tm_mon,d->tm_year,d->tm_hour,d->tm_min,d->tm_sec,d->tm_wday,d->tm_yday);
+    
+}
+
+static void test_datetime(void){
+    struct timespec t;
+    struct datetime d[1];
+    
+    timespec_runtime(&t,
+        unsigned int i=1000000;
+        while(i--)
+            datetime_from_epoch(d,time(NULL));
+    );
+    
+    printf("datetime %f ms: %d.%d.%d / %d:%d:%d / %d / %d\n",timespec_milliseconds(&t),
+    d->day,d->month,d->year,d->hour,d->minute,d->second,d->day_w,d->day_y);
+}
+
+
 int main(int argc, char **argv){
     test_timespec();
+    test_localtime();
+    test_datetime();
     test_timer();
     return 0;
 }
